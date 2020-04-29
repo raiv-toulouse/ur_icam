573f5516a1198f6755921b44d0733e14994981ac
raiv.toulouse@icam.fr <raiv.toulouse@icam.fr>
2020-04-29 09:37:49 +0200

    Quelques modifs pour meilleur fonctionnement de node_pyqt_interact_robot.py

--- ur_icam_description/src/ur_icam/reglage.py
+++ ur_icam_description/src/ur_icam/reglage.py
@@ -25,7 +25,7 @@ class Reglage(QWidget):
         self.slider.setSliderPosition(val)
         self.label.setText(text)
         self.edit.setText(str(val))
-        self.slider.setSliderPosition(self.invConvert(val))
+        self.slider.setSliderPosition(self._invConvert(val))
         self.slider.valueChanged.connect(self.changerValeurEdit)
         self.edit.editingFinished.connect(self.changerValeurSlider)
         self.setLayout(hLayout)
@@ -33,20 +33,43 @@ class Reglage(QWidget):
     def value(self):
         return float(self.edit.text())
 
-    def convert(self,pos):
+    def majEditEtSlider(self,value):
+        '''
+        Met à jour la valeur du slider et du edit d'après la valeur reçue
+        @param value: une réel dans [min,max]
+        @return:
+        '''
+        self.edit.blockSignals(True)  # On bloque les signaux émits lors d'un changement le temps de faire la màj sur les 2
+        self.slider.blockSignals(True)  # pour éviter des signaux-slots cycliques
+        self.edit.setText('{:.3}'.format(value))
+        self.slider.setSliderPosition(self._invConvert(value))
+        self.edit.blockSignals(False)
+        self.slider.blockSignals(False)
+
+    def _convert(self,pos):
+        '''
+        Méthode privée permettant de convertir une position d'un slider en sa valeur dans [min,max]
+        @param pos:
+        @return:
+        '''
         return pos * self.a + self.b
 
-    def invConvert(self,val):
+    def _invConvert(self,val):
+        '''
+        Méthode privée permettant de convertir une valeur dans [min,max] en une position sur le slider
+        @param val:
+        @return:
+        '''
         return int((val - self.b) / float(self.a))
 
     def changerValeurEdit(self):
         val = self.slider.value()
-        self.edit.setText('{:.3}'.format(self.convert(val)))
+        self.edit.setText('{:.3}'.format(self._convert(val)))
 
     def changerValeurSlider(self):
         try:
             val = float(self.edit.text())
-            self.slider.setSliderPosition(self.invConvert(val))
+            self.slider.setSliderPosition(self._invConvert(val))
         except ValueError:
             QMessageBox.error(self, "Error", "Must be a number")
 
